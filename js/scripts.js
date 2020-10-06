74c6ac2327d7cbb10710fe7fd6881feaeac9e274
Pepe Doval <pepellou@gmail.com>
2020-10-06 08:30:04 -0500

    WIP

--- js/scripts.js
+++ js/scripts.js
@@ -33,6 +33,23 @@
     });
     */
 
+    $.get("http://worldtimeapi.org/api/timezone/Europe/London", function(data) {
+        console.log(data.unixtime);
+
+        var date = new Date(data.unixtime * 1000);
+        // Hours part from the timestamp
+        var hours = date.getHours();
+        // Minutes part from the timestamp
+        var minutes = "0" + date.getMinutes();
+        // Seconds part from the timestamp
+        var seconds = "0" + date.getSeconds();
+
+        // Will display time in 10:30:23 format
+        var formattedTime = hours + ':' + minutes.substr(-2) + ':' + seconds.substr(-2);
+
+        console.log(formattedTime);
+    });
+
     var menu = $('#menu');
     var converters = $('#converters');
 
